ca50b73a68eee1839545a619af5f33235b242bdf
Ned Groom <ned.groom@gmail.com>
2021-12-27 10:55:24 +0000

    Mining rewards and transaction object

--- YT02pt3/README.md
+++ YT02pt3/README.md
@@ -6,7 +6,7 @@
 
 This folder is my workspace for going through the following youtube video:
 
-"Implementing Proof-of-Work in Javascript" - https://www.youtube.com/watch?v=HneatE69814
+"Mining rewards & transactions - Blockchain in Javascript Part 3" - https://www.youtube.com/watch?v=fRV6cGXVQ4I
 
 Author: "Simply Explained", (Xavier)
 
@@ -16,21 +16,28 @@ Author: "Simply Explained", (Xavier)
 
 Purpose:
 
- 
+    Expand blockchain implementation secured with proof of work algorithms, to allow each block to contain many transactions, and to add rewards for miners.
 
-----
+    Need virtual money or coins in the system, easiest way is to mine the coins inside of the system.
 
 
-Academic notes:
-
-   
-    
 
 ----
 
 
 Informal notes while doing:
 
-   
+    Bitcoin POW algorithm makes sure only one block created every 10 mins.
+
+    Not very impressed by this video, though just demonstrating simple concepts.
+
+    Instead of the mine block function, we have the mine pending transactions function.
+
+    We have also added rewards for mining a block.
+
+    At the end, we demonstrate that the rewards are added as a pending transaction so they will not be shown immediately.
+
+
+
 
 ----
--- YT02pt3/main.js
+++ YT02pt3/main.js
@@ -10,20 +10,30 @@
 const SHA256 = require('crypto-js/sha256');    //  "npm install --save crypto-js" to install node_modules 
 
 
+class Transaction{
+
+    constructor(fromAddress, toAddress, amount){
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+
+    
+}
+
 
 class Block{
 
-    constructor(index, timestamp, data, previousHash = ''){     // index tells us where the block sits on the chain
-        this.index = index;
+    constructor(timestamp, transactions, previousHash = ''){     // index tells us where the block sits on the chain
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
         this.nonce = 0;
     }
 
     calculateHash(){
-        return SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash + this.nonce).toString();
+        return SHA256(this.timestamp + JSON.stringify(this.data) + this.previousHash + this.nonce).toString();
     }
 
     mineBlock(difficulty){
@@ -42,21 +52,49 @@ class Blockchain{
 
     constructor(){
         this.chain = [this.createGenesisBlock()];     // an array that contains the genesis block.
-        this.difficulty = 4;
+        this.difficulty = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 100;
     }
 
     createGenesisBlock(){
-        return new Block(0, "21.12.21", "Genesis block", "0");
+        return new Block("27.12.21", "Genesis block", "0");
     }
 
     getLatestBlock(){
         return this.chain[this.chain.length - 1];
     }
 
-    addBlock(newBlock){
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);                          //  in reality you cannot add a block so easily. There are numerous checks in place.
+    minePendingTransaction(miningRewardAddress){
+        let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock.hash); // again, will not be adding all pending transactions.
+        block.mineBlock(this.difficulty);
+        console.log('Block successfully mined!');
+        this.chain.push(block);
+
+        this.pendingTransactions = [
+            new Transaction(null, miningRewardAddress, this.miningReward)
+        ];
+    }
+
+    createTransaction(transaction){
+        this.pendingTransactions.push(transaction);
+    }
+
+    getBalanceOfAddress(address){
+        let balance = 0;
+
+        for(const block of this.chain){
+            for(const trans of block.transactions){
+                if (trans.fromAddress === address){
+                    balance -= trans.amount;
+                }
+                if (trans.toAddress === address){
+                    balance += trans.amount;
+                }
+            }
+        }
+
+        return balance;
     }
 
     isChainValid(){
@@ -79,12 +117,15 @@ class Blockchain{
 
 let muffinCoin = new Blockchain();
 
-console.log('Mining block 1...');
-muffinCoin.addBlock(new Block(1, "23.12.21", {amount: 4}));      // previous hash instantiates at empty, and will update when block is added.
+muffinCoin.createTransaction( new Transaction('address1', 'address2', 100));  // addresses are the public keys of someones wallet.
+muffinCoin.createTransaction( new Transaction('address2', 'address1', 27));
 
-console.log('Mining block 2...');
-muffinCoin.addBlock(new Block(2, "24.12.21", {amount: 10}));
+console.log('\n Starting the miner...');
+muffinCoin.minePendingTransaction('neds address');
 
+console.log('\n neds balance is ' + muffinCoin.getBalanceOfAddress('neds address'));
 
+console.log('\n Starting the miner...');
+muffinCoin.minePendingTransaction('neds address');
 
-
+console.log('\n neds balance is ' + muffinCoin.getBalanceOfAddress('neds address'));
