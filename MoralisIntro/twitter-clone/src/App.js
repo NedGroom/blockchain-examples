faca0e3c6692565229922955a0a939ff7c62983e
Ned Groom <ned.groom@gmail.com>
2022-01-18 11:46:35 +0000

    updates

--- MoralisIntro/twitter-clone/src/App.js
+++ MoralisIntro/twitter-clone/src/App.js
@@ -2,15 +2,32 @@ import { useMoralis } from 'react-moralis';
 import { Button } from '@chakra-ui/react';
 import { Container, Heading } from '@chakra-ui/layout';
 import { Auth } from './Auth';
+import { Route, Switch } from "react-router-dom";
+import { Home } from './Home';
+import { Profile } from './Profile';
 
 
 function App() {
-  const { isAuthenticated, logout } = useMoralis();
+  const { isAuthenticated, logout, user } = useMoralis();
   
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/profile" exact>
+        <Profile />
+      </Route>
+    </Switch>
+  );
+
+  //<Route path='/welcome' element={<Home/>} />
+
+
   if(isAuthenticated){
     return (
       <Container>
-        <Heading>Welcome to the twitter clone</Heading>
+        <Heading>Welcome to the twitter clone, {user.attributes.username}</Heading>
         <Button onClick={()=> logout()}>Logout</Button>
       </Container>
     );  
